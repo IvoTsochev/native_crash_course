c53ddf05e3c4a5b065b9251641d98e696da9325c
Ivaylo Tsochev <ivo.tsochev@live.com>
2024-09-07 00:53:43 +0300

    initial commit

---
+++ app/search/[query].jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export const Search = (props) => {
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
